85755c1cda1d3d339080486f4747f4387b019308
Your Name <you@example.com>
2020-11-17 10:39:36 -0700

    Added loops and arrays

--- js/app.js
+++ js/app.js
@@ -1,20 +1,29 @@
-var age = prompt("What is your age?");
-var is_subscribed = confirm("Are you subscribed?");
+var userAges = [15, 25, 35, 45]
+var subscriptionStatus = [true, false, true, false];
 
 
 
+for(var index = 0; index < userAges.length; index++) {
+    var userAge = userAges[index];
+    var is_subscribed = subscriptionStatus[index];
 
-if (age < 18 && is_subscribed == false) {
-    console.log( "The user is younger than 18 and is not subscribed");
+    if (userAge < 18 && is_subscribed) {
+        console.log( "The user is younger than 18 and is not subscribed");
+    }
+    
+    else if (userAge >= 18 && is_subscribed) {
+        console.log("The user is 18 and older and not subscribed");
+    }
+    
+    else if (userAge < 18 && is_subscribed) {
+        console.log("The user is younger than 18 and is subscribed");
+    }
+    else {
+        console.log("The user is 18 and older and is subscribed");
+    }
+
 }
 
-else if (age >= 18 && is_subscribed == false) {
-    console.log("The user is 18 and older and not subscribed");
-}
 
-else if (age < 18 && is_subscribed == true) {
-    console.log("The user is younger than 18 and is subscribed");
-}
-else {
-    console.log("The user is 18 and older and is subscribed");
-}
+
+
